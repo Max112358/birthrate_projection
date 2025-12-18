bcb3fda916c9947d1c6750055895c4c37d7f2001
Max112358 <97316904+Max112358@users.noreply.github.com>
2025-12-17 19:06:26 -0800

    moving to new population distribution

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Load the dataframe
-df = pd.read_csv('life_expectancy.csv')
+df = pd.read_csv('population_distribution.csv')
 
 # Inspect the dataframe
 print(df.head())
 print(df.info())
 
 # Save to JSON
-json_filename = 'life_expectancy.json'
+json_filename = 'population_distribution.json'
 df.to_json(json_filename, orient='records', indent=4)
 
 print(f"File saved as {json_filename}")
